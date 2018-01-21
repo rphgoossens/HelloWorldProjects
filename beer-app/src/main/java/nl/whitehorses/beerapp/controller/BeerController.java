3de5f8fec111fafc121bcaae5229d1bea4e703a1
r.p.h.goossens@gmail.com <r.p.h.goossens@gmail.com>
2018-01-21 17:14:42 +0100

    added version number in endpoint

--- beer-app/src/main/java/nl/whitehorses/beerapp/controller/BeerController.java
+++ beer-app/src/main/java/nl/whitehorses/beerapp/controller/BeerController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/hello-beer/")
+@RequestMapping("/hello-beer/1.0")
 public class BeerController {
 
     @Autowired

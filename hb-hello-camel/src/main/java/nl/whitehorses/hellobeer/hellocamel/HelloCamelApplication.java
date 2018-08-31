3257e21b66390b88c49f2eb4cfb2f744c8335d05
r.p.h.goossens@gmail.com <r.p.h.goossens@gmail.com>
2018-08-31 13:53:08 +0200

    First setup

---
+++ hb-hello-camel/src/main/java/nl/whitehorses/hellobeer/hellocamel/HelloCamelApplication.java
@@ -0,0 +1,12 @@
+package nl.whitehorses.hellobeer.hellocamel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloCamelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloCamelApplication.class, args);
+    }
+}
